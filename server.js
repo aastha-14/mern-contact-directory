44f414f628c2df3f1a446a84c0b3c4281c26255b
Aastha Gupta <aastha1406@gmail.com>
2020-10-17 14:25:34 +0530

    Prepare for deploy

--- server.js
+++ server.js
@@ -1,22 +1,28 @@
-const express = require('express')
-const app = express()
-const db = require('./config/db')
-const morgan = require('morgan')
-const { urlencoded } = require('express')
-
-db()
+const express = require('express');
+const app = express();
+const db = require('./config/db');
+const morgan = require('morgan');
+const { urlencoded } = require('express');
+const path = require('path');
+db();
 
 if (app.get('env') === 'development') {
-    app.use(morgan('tiny'))
+    app.use(morgan('tiny'));
     console.log('Morgan enabled...');
 }
-app.use(urlencoded({ extended: true }))
+app.use(urlencoded({ extended: true }));
 app.use(express.json({ extended: false }));
 
-app.use('/api/users', require('./routes/users'))
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/contacts', require('./routes/contacts'))
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, () => console.log(`Server running on port: ${PORT}...`))
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}...`));
